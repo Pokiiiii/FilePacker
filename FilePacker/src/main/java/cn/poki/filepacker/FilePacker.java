d9afc0ff9df326dc5fad3755857d65a5f1ad194e
czfshine <czfshine@outlook.com>
2019-08-17 00:05:44 +0800

    代码框架完工

--- FilePacker/src/main/java/cn/poki/filepacker/FilePacker.java
+++ FilePacker/src/main/java/cn/poki/filepacker/FilePacker.java
@@ -50,22 +50,40 @@ public class FilePacker {
          * @param outputFile
          */
         public void saveToFile(File outputFile){
-            //todo answer14: 按什么顺序写，写什么，
+            //todo [task]-3:把伪代码变成代码
+            //要写的内容有
+            // 1.文件数量
+            // 2.对于每个文件
+            //  2.1 文件名长度
+            //  2.2 文件名
+            //  2.3 数据起始位置
+            //  2.4 数据长度
+            //  2.5 实际数据
             throw new NotImplementedException();
         }
 
         /**从数据文件读取数据块
          */
         public void loadFromFile(File inputFile){
-            //todo answer15: 按什么顺序读，读什么，
+            //todo [task]-4:把伪代码变成代码，顺序和save一样
             throw new NotImplementedException();
+        }
+
+        public void addBlock(FileBlock block){
+            //todo [task]-5:加到数组
         }
     }
 
     // 下面两个函数就是递归的时候分割文件和文件夹的（意义上）
     private void  analyzeFile(File filepath){
-        //todo answer13: 这个函数就是给 packInfos 增加元素的，
-        // 考虑一下什么情况下需要增加一个元素，什么情况下只需补充信息即可？
+        // todo [task]-2:把伪代码变成代码
+        // 当前文件的大小为 a，最后一个块的大小为b
+        // 如果a+b小于BLOCKSIZE
+        //    当前文件加到最后一个块
+        // else
+        //    当前文件的前半部分加到最后一个块
+        //    new 一个新的块加到数组（变成最后一块了)
+        //     当前文件的后半部分加到最后一个块
         throw new NotImplementedException();
     }
 
@@ -84,6 +102,7 @@ public class FilePacker {
 
 
     private void executePackage(List<PackInfo> packInfos){
+        //todo answer16: 这个函数应该知道怎么写了吧.. packInfos都给你了，saveToFile也有了
         throw new NotImplementedException();
     }
 
