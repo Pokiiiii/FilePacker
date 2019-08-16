ccf8eb99bebf9506220167165fe42fbf66a8d241
czfshine <czfshine@outlook.com>
2019-08-16 22:11:27 +0800

    常量不要出现在代码里面，独立出来

--- FilePacker/src/FilePacker/FilePacker.java
+++ FilePacker/src/FilePacker/FilePacker.java
@@ -1,12 +1,12 @@
 package FilePacker;
 
 import java.io.*;
-import java.util.Arrays;
-import javax.security.auth.DestroyFailedException;
 
 public class FilePacker {
-    private static void splitFile(File f, int eachSize) {
-        if(f.length()>10 * 1024 * 1024) {
+    public static final Long BLOCKSIZE=10 * 1024 * 1024L;//10MB
+    public static final String BASEPATH="c:\\Users\\73426\\Desktop\\PPT\\test";
+    private static void splitFile(File f, Long eachSize) {
+        if(f.length()>BLOCKSIZE) {
             int count = (int)(Math.ceil(f.length() / eachSize))+1;//块数
             try {
                 InputStream inf = new FileInputStream(f);
@@ -29,19 +29,19 @@ public class FilePacker {
                     outf[j] = new FileOutputStream(dir_f[j]);
                 }
                 /**写入各块内容*/
-                int s,m=0,  n=10*1024;
-                byte[] buffer = new byte[n];
-                s = inf.read(buffer, 0, n);
+                int s,m=0,  buffsize=10*1024;
+                byte[] buffer = new byte[buffsize];
+                s = inf.read(buffer, 0, buffsize);
                 while(s != -1&& m<count) {
-                    if(dir_f[m].length() < 10*1024) {
-                        outf[m].write(buffer, 0, n);
-                        s = inf.read(buffer, 0, n);
+                    if(dir_f[m].length() < buffsize) {
+                        outf[m].write(buffer, 0, buffsize);
+                        s = inf.read(buffer, 0, buffsize);
                     }
-                    if(dir_f[m].length() == 10*1024){
+                    if(dir_f[m].length() == buffsize){
                         outf[m].close();
                         m = m+1;
-                        int off = (int)(f.length()-m*10*1024);
-                        if(off<10*1024) {
+                        int off = (int)(f.length()-m*buffsize);
+                        if(off<buffsize) {
                             outf[m].write(buffer, 0, off);
                             outf[m].close();
                             break;
@@ -66,18 +66,18 @@ public class FilePacker {
     public static void divide( String fPath ) {
         File f = new File(fPath);
         if(f.exists()){
-            if(f.isFile() && f.length() > 10*1024*1024) {
-                splitFile(f, 10*1024*1024);
+            if(f.isFile() && f.length() > BLOCKSIZE) {
+                splitFile(f, BLOCKSIZE);
             }
-            else if(f.isDirectory() && f.length() > 10*1024*1024) {
+            else if(f.isDirectory() && f.length() > BLOCKSIZE) {
                 File[] dir = f.listFiles();
 
                 for(int i=0; i<dir.length; i++) {
-                    if(dir[i].exists() && dir[i].length() > 10*1024*1024){
+                    if(dir[i].exists() && dir[i].length() > BLOCKSIZE){
                         if(dir[i].isFile()) {
-                            splitFile(dir[i], 10*1024*1024);
+                            splitFile(dir[i], BLOCKSIZE);
                         }
-                        else if(dir[i].isDirectory() && dir[i].length() > 10*1024*1024) {
+                        else if(dir[i].isDirectory() && dir[i].length() > BLOCKSIZE) {
                             divide(dir[i].getAbsolutePath());
                         }
                     }
@@ -97,18 +97,19 @@ public class FilePacker {
                 return true;
             }
         };
-        File filePath = new File("c:\\Users\\73426\\Desktop\\PPT\\test\\作业要求");//需要拼装的目录
+        File filePath = new File(BASEPATH+"\\作业要求");//需要拼装的目录
         String fileIndex="0";
         File FileBig = new File(filePath + "/" + "Files"+"_"+fileIndex);
         BufferedOutputStream out=new BufferedOutputStream(new FileOutputStream(FileBig));
-
+        int buffsize=1024;
         children = dir.list(filter);
         for (int i = 0; i < children.length; i++) {
             String inhalt = children[i];
             FileInputStream in = new FileInputStream(dir + "/" + inhalt);
-            byte[] buffer = new byte[1024];
+
+            byte[] buffer = new byte[buffsize];
             int len=0;
-            if (FileBig.length() /1024 / 1024 < 10) {
+            if (FileBig.length() /buffsize / buffsize < 10) {
                 while ((len = in.read(buffer)) > 0) {
                     out.write(buffer, 0, len);
                 }
@@ -132,10 +133,9 @@ public class FilePacker {
 
     public static void start()
     {
-        String path = "c:\\Users\\73426\\Desktop\\PPT\\test";//要切割的文件/目录路径
-        divide(path);//切割
-        File dir = new File("c:\\Users\\73426\\Desktop\\PPT\\test\\作业要求");
-        File out=new File("c:\\Users\\73426\\Desktop\\PPT\\test\\作业要求");
+        divide(BASEPATH);//切割
+        File dir = new File(BASEPATH+"\\作业要求");
+        File out=new File(BASEPATH+"\\作业要求");
         try {
             murgeFile(dir,out);
             //由于是在网上找的方法，东拼西凑改的，合并这个地方还不够灵活，应该要再创建一个方法遍历原目录所有文件进行合并处理，遇到子文件夹再将子文件也拼接（待完成）
